fd67c5f326d36e37b0ae7e703bcaf5d24b5f38f3
79686 <mrak5455@yandex.ru>
2024-10-17 10:52:33 +0300

    добавил properties фаил и зделал подключение к бд

--- src/main/java/aleksey/Main.java
+++ src/main/java/aleksey/Main.java
@@ -1,12 +1,17 @@
 package aleksey;
 
 import aleksey.concoleinterface.MainMenu;
+import aleksey.utils.ConnectionManager;
 
 public class Main {
 
-    public static void main(String[] args) {
-        MainMenu mainMenu = new MainMenu();
-        mainMenu.start();
+    public static void main(String[] args) throws Exception {
+        //MainMenu mainMenu = new MainMenu();
+        //mainMenu.start();
+
+        try(var connection = ConnectionManager.open()) {
+            System.out.println(connection.getTransactionIsolation());
+        }
     }
 
 
--- src/main/java/aleksey/utils/ConnectionManager.java
+++ src/main/java/aleksey/utils/ConnectionManager.java
@@ -1,4 +1,27 @@
 package aleksey.utils;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public final class ConnectionManager {
+    private static final String URL = "db.uri";
+    private static final String USERNAME = "db.username";
+    private static final String PASSWORD = "db.password";
+
+    private ConnectionManager() {}
+
+    public static Connection open() {
+        try {
+            return DriverManager.getConnection(
+                    PropertiesUtils.get(URL),
+                    PropertiesUtils.get(USERNAME),
+                    PropertiesUtils.get(PASSWORD)
+            );
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
 }
